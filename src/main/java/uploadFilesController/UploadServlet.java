873c549fa477017afaa4f87336f0563b057dbb56
olga-muravjova <olyamuravyova@yandex.ru>
2015-11-08 18:21:36 +0300

    added new controller to upload files

---
+++ src/main/java/uploadFilesController/UploadServlet.java
@@ -0,0 +1,71 @@
+package uploadFilesController;
+
+/**
+ * Created by Юыќур on 05.11.2015.
+ */
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+
+@WebServlet("/UploadServlet")
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 2, // 2MB
+        maxFileSize = 1024 * 1024 * 10,      // 10MB
+        maxRequestSize = 1024 * 1024 * 50)   // 50MB
+public class UploadServlet extends HttpServlet
+{
+    /**
+     * Name of the directory where uploaded files will be saved, relative to
+     * the web application directory.
+     */
+    private static final String SAVE_DIR = "uploadFiles";
+
+    /**
+     * handles file upload
+     */
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+        // gets absolute path of the web application
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(savePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+
+        for (Part part : request.getParts()) {
+            String fileName = extractFileName(part);
+            if (fileName.contains("?")) {
+                fileName = fileName.replace("?", "0");
+            }
+            part.write(savePath + File.separator + fileName);
+        }
+
+        request.setAttribute("message", "Upload has been done successfully!");
+        getServletContext().getRequestDispatcher("/message.jsp").forward(
+                request, response);
+    }
+
+    /**
+     * Extracts file name from HTTP header content-disposition
+     */
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length() - 1);
+            }
+        }
+        return "";
+    }
+}
---
+++ target/analyzeme/Upload.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Ольга
+  Date: 04.11.2015
+  Time: 18:45
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+         pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <title>File Upload</title>
+</head>
+<body>
+<center>
+  <h1>File Upload</h1>
+  <form method="post" action="UploadServlet"
+        enctype="multipart/form-data">
+    Select file to upload: <input type="file" name="file" size="60" /><br />
+    <br /> <input type="submit" value="Upload" />
+  </form>
+</center>
+</body>
+</html>
