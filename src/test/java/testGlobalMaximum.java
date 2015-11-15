dabed05e907a9c8b0717758e6a9ca2c600e8d5f4
SKutukov <skutukov96@gmail.com>
2015-11-15 22:33:33 +0300

    test Class

---
+++ src/test/java/testGlobalMaximum.java
@@ -0,0 +1,23 @@
+import com.analyze.GlobalMaximum;
+import com.analyze.Point;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+
+/**
+ * Created by Sergey on 15.11.2015.
+ */
+public class testGlobalMaximum {
+
+    @Test
+    public void testGlobalMax() throws Exception{
+        GlobalMaximum tester = new GlobalMaximum();
+
+        assertEquals(4,4);
+
+
+
+
+    }
+
+}
---
+++ src/test/java/testGlobalMinimum.java
@@ -0,0 +1,17 @@
+import com.analyze.GlobalMinimum;
+import com.analyze.Point;
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+/**
+ * Created by Sergey on 15.11.2015.
+ */
+public class testGlobalMinimum {
+    @Test
+    public void testGlobalMin() throws Exception{
+        GlobalMinimum tester = new GlobalMinimum();
+
+        assertEquals(4,4);
+
+    }
+}
