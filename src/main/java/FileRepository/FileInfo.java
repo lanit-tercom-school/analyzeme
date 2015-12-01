7ecc7e7ad00a649489ddb666c9318b4b11506bb9
Andrey Kalikin <kalikin.andrey@gmail.com>
2015-12-01 18:46:41 +0300

    Added drag-and-drop pop-up file upload form [issue 11]

--- src/main/java/FileRepository/FileInfo.java
+++ src/main/java/FileRepository/FileInfo.java
@@ -7,32 +7,32 @@ import java.util.Date;
  * - nameToWrite implies startName
  * - nameToWrite implies uploadingDate
  * - startName && uploadingDate imply nameToWrite
- * <p>
+ * <p/>
  * For whole repository:
  * - nameToWrite implies user, startName, uploadingDate
  * - user && startName && uploadingDate imply nameToWrite
- * <p>
+ * <p/>
  * Created by Laetitia_Lagroffe on 27.11.2015.
  */
 public class FileInfo {
 
-	public String startName;
-	public String nameToWrite;
-	public Date uploadingDate;
-	public String login;
+    public String startName;
+    public String nameToWrite;
+    public Date uploadingDate;
+    public String login;
 
-	/**
-	 * @param startName   - name given by user
-	 * @param nameToWrite - name generated for repository
-	 */
-	FileInfo(final String startName, final String nameToWrite, final String login) {
-		this.startName = startName;
-		this.nameToWrite = nameToWrite;
-		this.login = login;
-		//default ctor fills Date with current info
-		uploadingDate = new Date();
-	}
+    /**
+     * @param startName   - name given by user
+     * @param nameToWrite - name generated for repository
+     */
+    FileInfo(final String startName, final String nameToWrite, final String login) {
+        this.startName = startName;
+        this.nameToWrite = nameToWrite;
+        this.login = login;
+        //default ctor fills Date with current info
+        uploadingDate = new Date();
+    }
 
-	FileInfo() {
-	}
+    FileInfo() {
+    }
 }
