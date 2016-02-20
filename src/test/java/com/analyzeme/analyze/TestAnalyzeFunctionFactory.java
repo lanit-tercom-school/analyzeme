1bb9181d683f34d60c3e51ef6e459271bf8e27a3
lagroffe <g.tomilina@yandex.ru>
2016-02-20 23:27:56 +0300

    json-parser restructured

---
+++ src/test/java/com/analyzeme/analyze/TestAnalyzeFunctionFactory.java
@@ -0,0 +1,33 @@
+package com.analyzeme.analyze;
+
+import com.analyzeme.analyze.AnalyzeFunction;
+import com.analyzeme.analyze.AnalyzeFunctionFactory;
+import com.analyzeme.analyze.GlobalMaximum;
+import com.analyzeme.analyze.GlobalMinimum;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+
+/**
+ * Test class of AnalyzeFunctionFactory
+ * Created by Sergey on 06.12.2015.
+ */
+public class TestAnalyzeFunctionFactory {
+
+    @Test
+    public void TestAnalyzeFunctionFactory() throws Exception {
+        AnalyzeFunctionFactory testFactory = new AnalyzeFunctionFactory();
+        //Create object of class GlobalMaximum.
+        AnalyzeFunction functionMax = testFactory.getFunction("GlobalMaximum");
+        //Test: If functionMax object of class GlobalMaximum?
+        assertTrue(functionMax instanceof GlobalMaximum);
+        //Create object of class GlobalMinimum.
+        AnalyzeFunction functionMin = testFactory.getFunction("GlobalMinimum");
+        //Test: If functionMin object of class GlobalMinimum?
+        assertTrue(functionMin instanceof GlobalMinimum);
+    }
+
+}
+
+
