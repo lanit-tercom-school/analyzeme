7a2e2a055ab50b915ef244fb7578ebf1f3c7a5b6
lagroffe <g.tomilina@yandex.ru>
2016-03-26 21:11:53 +0300

    Simple version of RParser #87 (if variables are valid in R)

--- src/main/java/com/analyzeme/R/facade/RParser.java
+++ src/main/java/com/analyzeme/R/facade/RParser.java
@@ -1,18 +1,31 @@
 package com.analyzeme.R.facade;
 
+import com.analyzeme.streamreader.StreamToString;
+
 import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lagroffe on 15.03.2016 22:32
  */
 
-/**
- * TODO: In the future parser should also be able to decide where to use data (from files and other sources) and complete script for execution
- */
-
 
 class RParser {
+	//if our special variables look like y_from__MyFile.json__
+	private static String regexp = ".?_from__([A-Za-z0-9-,]+.[a-z0-9]+)__";
+
+
+	public static void testParse() {
+		ArrayList<String> result = new ArrayList<String>();
+
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher m = pattern.matcher("here are some code \n {} : \n some other \n x_from_file.json_ \n y_from__MyData.json__ \n y <- c(x_from__MyJson.excel__)");
+		while (m.find()) {
+			System.out.println(m.group(1));
+		}
+	}
 
 	/**
 	 * finds out what files in the given project are necessary for the script to run
@@ -25,7 +38,14 @@ class RParser {
 	 */
 	public static ArrayList<String> parse(ByteArrayInputStream rScript, int userId, String projectId) throws Exception {
 		ArrayList<String> result = new ArrayList<String>();
-		//logic to be implemented
+
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher m = pattern.matcher(StreamToString.ConvertStream(rScript));
+		while (m.find()) {
+			System.out.println(m.group(1));
+			result.add(m.group(1));
+		}
+
 		return result;
 	}
 
@@ -40,7 +60,14 @@ class RParser {
 	 */
 	public static ArrayList<String> parse(String rCommand, int userId, String projectId) throws Exception {
 		ArrayList<String> result = new ArrayList<String>();
-		//logic to be implemented
+
+		Pattern pattern = Pattern.compile(regexp);
+		Matcher m = pattern.matcher(rCommand);
+		while (m.find()) {
+			System.out.println(m.group(1));
+			result.add(m.group(1));
+		}
+
 		return result;
 	}
 }
---
+++ src/test/java/com/analyzeme/R/facade/RParserTest.java
@@ -0,0 +1,14 @@
+package com.analyzeme.R.facade;
+
+import org.junit.Test;
+
+/**
+ * Created by lagroffe on 26.03.2016 21:07
+ */
+public class RParserTest {
+
+	@Test
+	public void testTest() {
+		RParser.testParse();
+	}
+}
