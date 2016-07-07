e17a88cd71b467c7ed02c84a95477962a90e0175
Ilya Kirillov <dzenin8bit@gmail.com>
2016-07-07 12:51:08 +0300

    Improved excel parser test

--- src/test/java/com/analyzeme/parsers/ExcelParserTest.java
+++ src/test/java/com/analyzeme/parsers/ExcelParserTest.java
@@ -6,16 +6,20 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+
+import static java.lang.System.in;
 
 /**
  * Created by ilya on 7/5/16.
@@ -42,11 +46,9 @@ public class ExcelParserTest {
             }
         }
 
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        workbook.write(out);
-        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
         ExcelParser parser = new ExcelParser();
-        DataArray<Double> dataArray = parser.parse(in);
+        DataArray<Double> dataArray = parser.parse(createInputStreamFromWorkbook(workbook));
+
 
         Assert.assertTrue(new HashSet<String>(Arrays.asList(columnTitles)).equals(dataArray.getData().get(0).getKeys()));
 
@@ -56,5 +58,33 @@ public class ExcelParserTest {
                 Assert.assertEquals(data.get(r)[c], doubleData.getData().get(columnTitles[c]));
             }
         }
+    }
+
+    @Test(expected = InvalidFileException.class)
+    public void testInvalidFile() throws IOException, InvalidFileException {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Sheet");
+
+        List<Double[]> data = new ArrayList<Double[]>();
+        for (int i = 0; i < 10; i++) {
+            data.add(new Double[]{(double) i, (double) (i * i)});
+        }
+
+        for (int r = 0; r < data.size(); r++) {
+            Row row = sheet.createRow(r);
+            for (int c = 0; c < 2; c++) {
+                row.createCell(c).setCellValue(data.get(r)[c]);
+            }
+        }
+
+        ExcelParser parser = new ExcelParser();
+        parser.parse(createInputStreamFromWorkbook(workbook));
+    }
+
+    private InputStream createInputStreamFromWorkbook(Workbook workbook) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        workbook.write(out);
+        ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        return in;
     }
 }
