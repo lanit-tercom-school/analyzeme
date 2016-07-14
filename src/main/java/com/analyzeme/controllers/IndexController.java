7d91cc93df519f3c2502230e47e86275d8930692
lagroffe <g.tomilina@yandex.ru>
2016-07-14 13:42:59 +0300

    test for logging system #208

--- src/main/java/com/analyzeme/controllers/IndexController.java
+++ src/main/java/com/analyzeme/controllers/IndexController.java
@@ -2,32 +2,19 @@ package com.analyzeme.controllers;
 
 import com.analyzeme.rconfiguration.RConfRepository;
 import com.analyzeme.repository.UsersRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
-    @RequestMapping(value = "/")
-    public ModelAndView index() throws Exception {
-        ModelAndView mav = new ModelAndView("index");
+    private String returnIndex() throws Exception {
+        Logger logger = LoggerFactory.getLogger(
+                "com.analyzeme.controllers.IndexController");
+        logger.debug("Index page");
 
-        String msg = "Running IndexController.index() method";
-
-        UsersRepository.checkInitializationAndCreate();
-        try {
-            UsersRepository.findUser("guest");
-        } catch (IllegalArgumentException e) {
-            //login, email, password  (IN THIS ORDER)
-            String[] param = {"guest", "guest@mail.sth", "1234"};
-            UsersRepository.newItem(param);
-        }
-        mav.addObject("msg", msg);
-        return mav;
-    }
-
-    @RequestMapping(value = "/index")
-    public String moveToIndexPage() throws Exception {
         UsersRepository.checkInitializationAndCreate();
         try {
             UsersRepository.findUser("guest");
@@ -39,32 +26,41 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping(value = "/projects")
-    public String moveToProjectPage() {
-        return "projects";
+    @RequestMapping(value = "/")
+    public String index() throws Exception {
+        return returnIndex();
+    }
+
+    @RequestMapping(value = "/index")
+    public String moveToIndexPage() throws Exception {
+        return returnIndex();
     }
 
     @RequestMapping(value = "/data/spb")
     public String moveToPreviewPage() {
+        Logger logger = LoggerFactory.getLogger(
+                "com.analyzeme.controllers.IndexController");
+        logger.debug("Prewiew page");
         return "preview";
-    }
-
-    @RequestMapping(value = "/fileInfo")
-    public String moveToFileInfo() {
-        return "fileInfo";
     }
 
     @RequestMapping(value = "/config")
     public ModelAndView moveToRConfPage() {
-
-        String RConfList = RConfRepository.getRepo().allConfigurationsToJsonString();
-        return new ModelAndView("config", "RConfList", RConfList);
+        Logger logger = LoggerFactory.getLogger(
+                "com.analyzeme.controllers.IndexController");
+        logger.debug("Configs page");
+        String RConfList = RConfRepository.getRepo()
+                .allConfigurationsToJsonString();
+        return new ModelAndView("config",
+                "RConfList", RConfList);
 
     }
 
-
     @RequestMapping(value = "/help")
     public String moveToHelp() {
+        Logger logger = LoggerFactory.getLogger(
+                "com.analyzeme.controllers.IndexController");
+        logger.debug("Help page");
         return "help";
     }
 
