383a832654bfee283bb6f9b15fda1d9ef3a1ec67
Ilya Kirillov <dzenin8bit@gmail.com>
2016-07-18 12:46:17 +0300

    Added Date and DateTime to DateTimeUtils

--- src/main/java/com/analyzeme/parsers/DateAndTimeUtils.java
+++ src/main/java/com/analyzeme/parsers/DateAndTimeUtils.java
@@ -1,5 +1,7 @@
 package com.analyzeme.parsers;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -11,56 +13,59 @@ import java.util.Map;
 public class DateAndTimeUtils {
 
     //Taken from here http://stackoverflow.com/a/3390252/5494618
-//    private static final Map<String, String> DATE_AND_TIME_FORMAT_REGEXPS = new HashMap<String, String>() {{
-//        put("^\\d{8}$", "yyyyMMdd");
-//        put("^\\d{1,2}-\\d{1,2}-\\d{4}$", "dd-MM-yyyy");
-//        put("^\\d{4}-\\d{1,2}-\\d{1,2}$", "yyyy-MM-dd");
-//        put("^\\d{1,2}/\\d{1,2}/\\d{4}$", "MM/dd/yyyy");
-//        put("^\\d{4}/\\d{1,2}/\\d{1,2}$", "yyyy/MM/dd");
-//        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$", "dd MMM yyyy");
-//        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$", "dd MMMM yyyy");
-//        put("^\\d{12}$", "yyyyMMddHHmm");
-//        put("^\\d{8}\\s\\d{4}$", "yyyyMMdd HHmm");
-//        put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$", "dd-MM-yyyy HH:mm");
-//        put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy-MM-dd HH:mm");
-//        put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$", "MM/dd/yyyy HH:mm");
-//        put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy/MM/dd HH:mm");
-//        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMM yyyy HH:mm");
-//        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMMM yyyy HH:mm");
-//        put("^\\d{14}$", "yyyyMMddHHmmss");
-//        put("^\\d{8}\\s\\d{6}$", "yyyyMMdd HHmmss");
-//        put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd-MM-yyyy HH:mm:ss");
-//        put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy-MM-dd HH:mm:ss");
-//        put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "MM/dd/yyyy HH:mm:ss");
-//        put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy/MM/dd HH:mm:ss");
-//        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMM yyyy HH:mm:ss");
-//        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMMM yyyy HH:mm:ss");
-//    }};
     private static final Map<String, String> TIME_REGEXPS = new HashMap<String, String>() {{
         put("\\d{2}:\\d{2}$", "HH:mm");
         put("\\d{2}:\\d{2}:\\d{2}$", "HH:mm:ss");
     }};
 
-    private static String determineTimeFormat(String dateString) {
-        for (String regexp : TIME_REGEXPS.keySet()) {
+    private static final Map<String, String> DATE_REGEXPS = new HashMap<String, String>() {{
+        put("^\\d{1,2}-\\d{1,2}-\\d{4}$", "dd-MM-yyyy");
+        put("^\\d{4}-\\d{1,2}-\\d{1,2}$", "yyyy-MM-dd");
+        put("^\\d{1,2}/\\d{1,2}/\\d{4}$", "MM/dd/yyyy");
+        put("^\\d{4}/\\d{1,2}/\\d{1,2}$", "yyyy/MM/dd");
+        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}$", "dd MMM yyyy");
+        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}$", "dd MMMM yyyy");
+    }};
+
+    private static final Map<String, String> DATE_TIME_REGEXPS = new HashMap<String, String>() {{
+        put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}$", "dd-MM-yyyy HH:mm");
+        put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy-MM-dd HH:mm");
+        put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}$", "MM/dd/yyyy HH:mm");
+        put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}$", "yyyy/MM/dd HH:mm");
+        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMM yyyy HH:mm");
+        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}$", "dd MMMM yyyy HH:mm");
+
+        put("^\\d{1,2}-\\d{1,2}-\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd-MM-yyyy HH:mm:ss");
+        put("^\\d{4}-\\d{1,2}-\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy-MM-dd HH:mm:ss");
+        put("^\\d{1,2}/\\d{1,2}/\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "MM/dd/yyyy HH:mm:ss");
+        put("^\\d{4}/\\d{1,2}/\\d{1,2}\\s\\d{1,2}:\\d{2}:\\d{2}$", "yyyy/MM/dd HH:mm:ss");
+        put("^\\d{1,2}\\s[a-z]{3}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMM yyyy HH:mm:ss");
+        put("^\\d{1,2}\\s[a-z]{4,}\\s\\d{4}\\s\\d{1,2}:\\d{2}:\\d{2}$", "dd MMMM yyyy HH:mm:ss");
+    }};
+
+    private static String determineFormat(String dateString, Map<String, String> regexps) {
+        for (String regexp : regexps.keySet()) {
             if (dateString.toLowerCase().matches(regexp)) {
-                return TIME_REGEXPS.get(regexp);
+                return regexps.get(regexp);
             }
         }
         return null;
     }
 
+    /*
+     * For time
+     */
     public static boolean isValidTime(String time) {
         try {
-            parse(time);
+            parseTime(time);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
 
-    public static LocalTime parse(String time) throws IllegalArgumentException {
-        String format = determineTimeFormat(time);
+    public static LocalTime parseTime(String time) throws IllegalArgumentException {
+        String format = determineFormat(time, TIME_REGEXPS);
         if (format == null) {
             throw new IllegalArgumentException();
         } else {
@@ -69,6 +74,59 @@ public class DateAndTimeUtils {
                 return LocalTime.parse(time, formatter);
             } catch (Exception e) {
                 throw new IllegalArgumentException("Error while parsing time");
+            }
+        }
+    }
+
+    /*
+     * For date
+     */
+    public static boolean isValidDate(String date) {
+        try {
+            parseDate(date);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static LocalDate parseDate(String date) throws IllegalArgumentException {
+        String format = determineFormat(date, DATE_REGEXPS);
+        if (format == null) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+                return LocalDate.parse(date, formatter);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Error while parsing date");
+            }
+        }
+    }
+
+
+    /*
+     * For dateTime
+     */
+    public static boolean isValidDateTime(String dateTime) {
+        try {
+            parseDateTime(dateTime);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static LocalDateTime parseDateTime(String dateTime) throws IllegalArgumentException {
+        String format = determineFormat(dateTime, DATE_TIME_REGEXPS);
+        if (format == null) {
+            throw new IllegalArgumentException();
+        } else {
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+                return LocalDateTime.parse(dateTime, formatter);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Error while parsing dateTime");
             }
         }
     }
--- src/test/java/com/analyzeme/parsers/DateTimeUtilsTest.java
+++ src/test/java/com/analyzeme/parsers/DateTimeUtilsTest.java
@@ -3,23 +3,84 @@ package com.analyzeme.parsers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+
 /**
  * Created by ilya on 7/14/16.
  */
 public class DateTimeUtilsTest {
+    /*
+     * Time
+     */
     @Test()
-    public void testIsForValidDates() {
-        Assert.assertTrue(DateAndTimeUtils.isValidTime("10:20"));
+    public void testIsValidTime() {
         Assert.assertTrue(DateAndTimeUtils.isValidTime("10:20:30"));
     }
 
     @Test()
-    public void testIsValidDateValidFormatButInvalidDate() {
+    public void testIsValidTimeForValidFormatInvalidTime() {
         Assert.assertFalse(DateAndTimeUtils.isValidTime("30:20"));
     }
 
     @Test()
-    public void testIsValidDateInvalidDateFormat() {
+    public void testIsValidTimeForInvalidFormat() {
         Assert.assertFalse(DateAndTimeUtils.isValidTime("10:e3"));
     }
+
+    @Test()
+    public void testParseTime() {
+        Assert.assertEquals(LocalTime.of(10, 20, 30), DateAndTimeUtils.parseTime("10:20:30"));
+    }
+
+    /*
+     * Date
+     */
+    @Test()
+    public void testIsForValidDate() {
+        Assert.assertTrue(DateAndTimeUtils.isValidDate("10-10-2010"));
+    }
+
+    @Test()
+    public void testIsValidDateForValidFormatInvalidDate() {
+        Assert.assertFalse(DateAndTimeUtils.isValidTime("30-2-2010"));
+    }
+
+    @Test()
+    public void testIsValidDateForInvalidFormat() {
+        Assert.assertFalse(DateAndTimeUtils.isValidTime("30.2-2010"));
+    }
+
+    @Test()
+    public void testParseDate() {
+        Assert.assertEquals(LocalDate.of(2010, Month.OCTOBER, 10), DateAndTimeUtils.parseDate("10-10-2010"));
+    }
+
+    /*
+    * DateTime
+    */
+    @Test()
+    public void testIsForValidDateTime() {
+        Assert.assertTrue(DateAndTimeUtils.isValidDateTime("10-10-2010 10:10"));
+    }
+
+    @Test()
+    public void testIsValidDateTimeForValidFormatInvalidDateTime() {
+        Assert.assertFalse(DateAndTimeUtils.isValidTime("30-2-2010 40:40:12"));
+    }
+
+    @Test()
+    public void testIsValidDatetimeForInvalidFormat() {
+        Assert.assertFalse(DateAndTimeUtils.isValidTime("30.2-2010 12:03"));
+    }
+
+    @Test()
+    public void testParseDateTime() {
+        Assert.assertEquals(LocalDateTime.of(2010, Month.OCTOBER, 10, 10, 10),
+                DateAndTimeUtils.parseDateTime("10-10-2010 10:10"));
+    }
+
+
 }
