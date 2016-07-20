2e2c28a61248a398e5888e378d1b82003577e5b7
lagroffe <g.tomilina@yandex.ru>
2016-07-20 15:12:40 +0300

    bat file

---
+++ before_commit.bat
@@ -0,0 +1,7 @@
+@rem *****************************************
+@rem Script for configs file auto-forming
+@rem *****************************************
+
+@echo Script-list script
+@cd r
+@dir /b /a-d > ../rscripts_info.txt
